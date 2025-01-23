250e872ef2d59e078e8c1417af8d6ab9218880af
Maxime Wang <maxime.wang@efrei.net>
2025-01-23 21:08:43 +0100

    Add dependencies & enabled loadbalancer in microservices

--- microservice-user_management/src/main/java/org/efrei/clients/AuthServiceClient.java
+++ microservice-user_management/src/main/java/org/efrei/clients/AuthServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "microservice-authentification")
+@FeignClient(name = "authentification", url = "http://authentification:8081")
 public interface AuthServiceClient {
     @GetMapping("/auth/ping/{token}")
     String ping(@RequestParam("token") String token);
